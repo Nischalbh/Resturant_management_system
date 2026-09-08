2d0d4af5e260ab25a1934bfa3375b972a80123f8
Nischal <nischalbhattarai408@gmail.com>
2026-09-08 07:52:41 +0545

    Add primary key to category URL

--- resturant_management_system/urls.py
+++ resturant_management_system/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from .views import category, Table
+from .views import category, Table, categorydetail
 
-urlpatterns = [path("category/", category), path("table/", Table)]
+urlpatterns = [
+    path("category/", category),
+    path("category/<int:pk>/", categorydetail),
+    path("table/", Table),
+]

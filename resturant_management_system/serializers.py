9b6c25b7003e19fcdc51c64b7839955dba82ddfa
Nischal <nischalbhattarai408@gmail.com>
2026-09-08 07:56:23 +0545

    Implement category create and update methods

--- resturant_management_system/serializers.py
+++ resturant_management_system/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from .models import Category
 
 
 class categorySerializers(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField()
+
+    def Create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validdated_data):
+        instance.name = validdated_data.get("name", instance.name)
+        return instance
 
 
 class TableSerializers(serializers.Serializer):
